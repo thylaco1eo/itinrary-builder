bc1c3ddb8d0b39a03ac4b310c03d98509288205e
thylaco1eo <frank.tancf@gmail.com>
2026-03-15 20:39:52 +0800

    20260315 complete flights in memory

---
+++ src/memory/core.rs
@@ -0,0 +1,75 @@
+use std::collections::HashMap;
+use surrealdb::{Surreal, engine::any};
+use surrealdb_types::{ToSql};
+use crate::domain::airport::{Airport, AirportCode};
+use crate::Infrastructure::db::model::flight_row::FlightRow;
+use crate::Infrastructure::db::repository::flight_repo::get_flights;
+use crate::Infrastructure::db::repository::airport_repo::get_all_airports;
+use crate::domain::flight::Flightcore;
+
+pub struct WebData {
+    database: Surreal<any::Any>,
+    flights: HashMap<String, Flightcore>,
+    airports: HashMap<String, Airport>
+}
+
+impl WebData {
+    pub async fn new(data_base: Surreal<any::Any>) -> Self {
+        let mut flights = HashMap::new();
+        let mut airports = HashMap::new();
+
+        let airport_rows = get_all_airports(&data_base).await;
+        for row in airport_rows {
+            let code = row.code.code.clone();
+            match Airport::try_from(row) {
+                Ok(airport) => {
+                    airports.insert(code, airport);
+                }
+                Err(_) => {}
+            }
+        }
+
+        let flight_rows: Vec<FlightRow> = get_flights(&data_base).await;
+        for row in flight_rows {
+            if let Some(e) = try_from(row.clone(), &airports) {
+                flights.insert(row.id.key.to_sql(), e);
+            }
+        }
+
+        WebData {
+            database: data_base,
+            flights,
+            airports
+        }
+    }
+
+    pub fn database(&self) -> &Surreal<any::Any> {
+        &self.database
+    }
+
+    pub fn flights(&self) -> &HashMap<String, Flightcore> {
+        &self.flights
+    }
+
+    pub fn airports(&self) -> &HashMap<String, Airport> {
+        &self.airports
+    }
+}
+
+fn try_from(row: FlightRow,airports:&HashMap<String,Airport>) -> Option<Flightcore> {
+    if !airports.contains_key(&row.origin_code) || !airports.contains_key(&row.destination_code){
+        return None;
+    }
+    let origin_tz = airports.get(&row.origin_code).unwrap().timezone();
+    let destination_tz = airports.get(&row.destination_code).unwrap().timezone();
+    let flight = Flightcore::new(
+        row.company,
+        row.flight_num,
+        AirportCode::new(row.origin_code).unwrap(),
+        AirportCode::new(row.destination_code).unwrap(),
+        row.dep_local.with_timezone(&origin_tz),
+        row.arr_local.with_timezone(&destination_tz),
+        row.block_time_minutes
+    );
+    Some(flight)
+}
